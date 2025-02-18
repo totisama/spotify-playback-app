d64ba9038689f6f6590823b967b9fb2b3d4c5979
Rodrigo Samayoa <toti1998sama@gmail.com>
2025-02-18 14:41:51 +0100

    add basic app layout

---
+++ src/shared/layout/AsideMenu.tsx
@@ -0,0 +1,8 @@
+export const AsideMenu = () => {
+  return (
+    <nav className='flex flex-1 flex-col gap-2'>
+      <div className='rounded-lg p-1'></div>
+      <div className='flex-1 rounded-lg p-2'></div>
+    </nav>
+  );
+};
---
+++ src/shared/layout/Player.tsx
@@ -0,0 +1,7 @@
+export const Player = () => {
+  return (
+    <>
+      <h2>Current song</h2>
+    </>
+  );
+};
