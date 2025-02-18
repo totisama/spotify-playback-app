ca2a65e1bfdfec6cb23d54f2d2c80bc9af72ea9c
Rodrigo Samayoa <toti1998sama@gmail.com>
2025-02-18 21:43:51 +0100

    add shadcn + input component

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,9 +12,13 @@ const config: Config = {
         background: 'var(--background)',
         foreground: 'var(--foreground)',
         'section-background': 'var(--section-background)',
+      borderRadius: {
+        lg: 'var(--radius)',
+        md: 'calc(var(--radius) - 2px)',
+        sm: 'calc(var(--radius) - 4px)',
       },
     },
   },
-  plugins: [],
+  plugins: [require('tailwindcss-animate')],
 };
 export default config;
