2983b0ff74f9ab5ec5f9f3eec2d6158f103d5ef1
Rodrigo Samayoa <toti1998sama@gmail.com>
2025-02-18 22:59:58 +0100

    Add SearchInput component

--- src/shared/layout/Header.tsx
+++ src/shared/layout/Header.tsx
@@ -1,13 +1,16 @@
 import { SpotifyIcon } from '@/shared/design/icons';
+import { SearchInput } from '@/shared/components/SearchInput';
 import Link from 'next/link';
 
 export const Header = () => {
   return (
-    <div className='flex w-full items-center p-2'>
+    <div className='flex h-full w-full items-center px-2'>
       <Link href='/'>
         <SpotifyIcon className='h-8 w-8' />
       </Link>
-      <div className='flex flex-grow justify-center'></div>
+      <div className='flex flex-grow justify-center'>
+        <SearchInput delay={300} />
+      </div>
       <Link href='/'>
         <SpotifyIcon className='h-8 w-8' />
       </Link>
