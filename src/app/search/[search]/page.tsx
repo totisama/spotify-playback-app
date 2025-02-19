bce44092cddcfa17a1f271177eba8885b23f23b8
Rodrigo Samayoa <toti1998sama@gmail.com>
2025-02-19 13:20:04 +0100

    Fix folder structure

--- src/app/search/[search]/page.tsx
+++ src/app/search/[search]/page.tsx
@@ -1,5 +1,5 @@
 import { type GeneralSearchResponse } from '@/shared/types/generalSearch';
-import GeneralSearch from '@/shared/design/GeneralSearch';
+import GeneralSearch from '@/shared/design/views/GeneralSearch';
 
 export default async function SearchPage({
   params,
