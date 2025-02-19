3edabd33169189d2ab7dd9479cad097013fae14c
Rodrigo Samayoa <toti1998sama@gmail.com>
2025-02-19 15:29:58 +0100

    Display PlayButton

--- src/shared/design/components/items/TrackItem.tsx
+++ src/shared/design/components/items/TrackItem.tsx
@@ -1,17 +1,21 @@
 import Image from 'next/image';
 import { type TracksItem } from '@/shared/types/generalSearch';
 import { formatDuration } from '@/shared/lib/formatDuration';
+import { PlayButton } from '@/shared/design/components/globals/PlayButton';
 
 export const TrackItem = ({ track }: { track: TracksItem }) => {
   return (
-    <li className='flex flex-col items-center gap-4 rounded-lg bg-black p-4 text-center transition hover:bg-black/40 md:flex-row md:text-start'>
-      <Image
-        src={track.album.images[0]?.url || '/placeholder.svg'}
-        alt={track.name}
-        width={50}
-        height={50}
-        className='rounded-md'
-      />
+    <li className='group flex flex-col items-center gap-4 rounded-lg bg-black p-4 text-center transition hover:bg-black/40 md:flex-row md:text-start'>
+      <div className='relative'>
+        <Image
+          src={track.album.images[0]?.url || '/placeholder.svg'}
+          alt={track.name}
+          width={50}
+          height={50}
+          className='rounded-md'
+        />
+        <PlayButton uri={track.uri} />
+      </div>
       <div className='flex-1'>
         <p className='text-md font-semibold'>{track.name}</p>
         <p className='text-sm text-gray-400'>
