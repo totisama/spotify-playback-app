018b8ce0dfffa38709aa87700e01ae2fc8d4dba1
Rodrigo Samayoa <toti1998sama@gmail.com>
2025-02-20 23:49:57 +0100

    Display followed artists on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,37 @@
-export default function HomePage() {
+import { Error } from '@/shared/design/components/globals/Error';
+import { type FollowingArtistsResponse } from '@/shared/types/spotifyTypes';
+import { Section } from '@/shared/design/layout/Section';
+import { ArtistItem } from '@/shared/design/components/items/ArtistItem';
+
+export default async function HomePage() {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_SITE_URL}/api/following`
+  );
+
+  if (!response.ok) {
+    return <Error text='Failed to fetch followed artists' />;
+  }
+
+  const data: FollowingArtistsResponse = await response.json();
+  const artists = data.following.items;
+
   return (
-    <div>
-      <h1>Spotify Web Playback</h1>
-    </div>
+    <Section
+      title='Followed Artists'
+      customClass='p-6'
+    >
+      {artists.length === 0 ? (
+        <p className='text-gray-400'>You are not following any artists.</p>
+      ) : (
+        <ul className='grid grid-cols-2 gap-6 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5'>
+          {artists.map((artist) => (
+            <ArtistItem
+              key={artist.id}
+              artist={artist}
+            />
+          ))}
+        </ul>
+      )}
+    </Section>
   );
 }
