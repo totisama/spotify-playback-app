544247618021218be79c229334b3bcfd99c31005
Rodrigo Samayoa <toti1998sama@gmail.com>
2025-02-20 14:53:23 +0100

    prop to add custom styles

--- src/shared/design/layout/Section.tsx
+++ src/shared/design/layout/Section.tsx
@@ -1,14 +1,17 @@
+import { cn } from '@/shared/lib/utils';
 import { type ReactNode } from 'react';
 
 export const Section = ({
   title,
   children,
+  customClass = '',
 }: {
   title: string;
   children: ReactNode;
+  customClass?: string;
 }) => {
   return (
-    <section className='mb-8'>
+    <section className={cn('mb-8', customClass)}>
       <h2 className='mb-4 text-2xl font-bold'>{title}</h2>
       {children}
     </section>
