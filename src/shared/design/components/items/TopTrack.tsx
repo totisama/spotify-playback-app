0f3256ad4a34bf9dcb326e95f5da7aca0a11dff2
Rodrigo Samayoa <toti1998sama@gmail.com>
2025-02-21 11:03:57 +0100

    add missing PlayButton

--- src/shared/design/components/items/TopTrack.tsx
+++ src/shared/design/components/items/TopTrack.tsx
@@ -1,17 +1,28 @@
 import Image from 'next/image';
 import { formatDuration } from '@/shared/lib/formatDuration';
 import { type Track } from '@/shared/types/spotifyTypes';
+import PlayButton from '../globals/PlayButton';
+import { PlayIcon } from 'lucide-react';
 
 export const TopTrack = ({ topTrack }: { topTrack: Track }) => {
   return (
-    <div className='flex flex-col items-center gap-4 rounded-lg bg-black p-6 text-center transition hover:bg-black/40 md:flex-row md:text-start'>
-      <Image
-        src={topTrack.album.images[0]?.url || '/images/placeholder.webp'}
-        alt={topTrack.name}
-        width={80}
-        height={80}
-        className='rounded-md'
-      />
+    <div className='group flex flex-col items-center gap-4 rounded-lg bg-black p-6 text-center transition hover:bg-black/40 md:flex-row md:text-start'>
+      <div className='relative '>
+        <Image
+          src={topTrack.album.images[0]?.url || '/images/placeholder.webp'}
+          alt={topTrack.name}
+          width={80}
+          height={80}
+          className='rounded-md'
+        />
+        <PlayButton
+          mode='track'
+          uri={topTrack.uri}
+          size='small'
+        >
+          <PlayIcon className='h-5 w-5 fill-white' />
+        </PlayButton>
+      </div>
       <div>
         <p className='text-lg font-bold'>{topTrack.name}</p>
         <p className='text-sm text-gray-400'>
