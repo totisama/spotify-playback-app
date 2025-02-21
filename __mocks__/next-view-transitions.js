92cc651e1cca687fe11d5ca84a4650f295146639
Rodrigo Samayoa <toti1998sama@gmail.com>
2025-02-21 11:43:05 +0100

    fix tests

---
+++ __mocks__/next-view-transitions.js
@@ -0,0 +1,5 @@
+module.exports = {
+  ViewTransitions: {
+    ViewTransitionLink: ({ children }) => children,
+  },
+};
--- jest.config.js
+++ jest.config.js
@@ -3,11 +3,13 @@ const nextJest = require('next/jest');
 const createJestConfig = nextJest({ dir: './' });
 
 const customJestConfig = {
-  testEnvironment: 'jest-environment-jsdom',
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   moduleNameMapper: {
-    '^@/components/(.*)$': '<rootDir>/src/components/$1',
     '^@/shared/(.*)$': '<rootDir>/src/shared/$1',
+    '^@/types/(.*)$': '<rootDir>/src/types/$1',
+    'next-view-transitions': '<rootDir>/__mocks__/next-view-transitions.js',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node', 'd.ts'],
 };
